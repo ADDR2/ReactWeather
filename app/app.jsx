0e2009e76dc33d50a3361f93b329ac8650030b14
Amaro <amarito93@gmail.com>
2017-06-15 21:29:57 -0400

    Add custom styles

--- app/app.jsx
+++ app/app.jsx
@@ -10,6 +10,9 @@ import { Route, Router, IndexRoute, hashHistory } from 'react-router';
 require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
+//App css
+require('style!css!applicationStyles');
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
